11970d160ad76c03eb4b462f00ceb1a4aa0e8823
JesusIsraelPradoPineda <83649450+JesusIsraelPradoPineda@users.noreply.github.com>
2021-06-09 08:30:07 -0500

    this is a fcommit

--- Ex01_09_06_2021.c
+++ Ex01_09_06_2021.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
-
+//only I am going to print my name and some data mine
 int main(void) {
   printf("Name: Jesus Israel Prado Pineda\n");
   printf("Social securuty number: 984876161616\n");
