7ebfb6ffa26dfe7d052b0b2cde1eba51a564d303
JesusIsraelPradoPineda <83649450+JesusIsraelPradoPineda@users.noreply.github.com>
2021-06-09 08:32:36 -0500

    this is a fcommit

---
+++ Ex02_09_06_2021.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+//in this program i will print a "E" whit asteriscos"
+
+int main(void) {
+  printf("*****\n");
+  printf("*\n");
+  printf("*\n");
+  printf("*****\n");
+  printf("*\n");
+  printf("*\n");
+  printf("*****\n");
+
+  return 0;
+}
