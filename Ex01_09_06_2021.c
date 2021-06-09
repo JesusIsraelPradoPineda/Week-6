250b11b18d300e9077938be21abc82895e6421cd
JesusIsraelPradoPineda <83649450+JesusIsraelPradoPineda@users.noreply.github.com>
2021-06-09 08:29:16 -0500

    this is a fcommit

---
+++ Ex01_09_06_2021.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main(void) {
+  printf("Name: Jesus Israel Prado Pineda\n");
+  printf("Social securuty number: 984876161616\n");
+  printf("Date of birth: January 17, 2002\n");
+
+  return 0;
+}
