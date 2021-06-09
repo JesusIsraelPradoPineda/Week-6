420123a17d235875c2c9bc87eae942556175e85b
JesusIsraelPradoPineda <83649450+JesusIsraelPradoPineda@users.noreply.github.com>
2021-06-09 09:02:41 -0500

    this si a fcommit

---
+++ Ex05_09_06_2021.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main(void) {
+  int A;
+  float B;
+  int C;
+ printf ("dame un numero con punto decimal\n");
+ scanf  ("%d",&A);
+ printf ("dame un numero con entero\n");
+ scanf  ("%f",&B);
+ printf ("dame un caracter cualquiera\n");
+ scanf  ("%d",&C);
+ printf ("tu resultado es %d\n",A);
+ printf ("tu resultado es %f\n",B);
+ printf ("tu resultado es %d\n",C);
+  return 0;
+}
